a3544bb084a0cf848957c8737b6df5f6b990f27e
Dharaneesh0912 <dharaneeshs0912@gmail.com>
2026-07-09 15:05:33 +0530

    Create 2812-FindTheMaximumAchievableNumber

---
+++ 2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.cpp
@@ -0,0 +1,9 @@
+// Last updated: 7/9/2026, 3:05:31 PM
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        int result=2*t;
+        return num + result;
+        
+    }
+};
