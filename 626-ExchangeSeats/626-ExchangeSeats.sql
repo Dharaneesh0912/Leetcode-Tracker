c18e28c13b0feb95e294544eb88c069b84d52d2a
Dharaneesh0912 <dharaneeshs0912@gmail.com>
2026-07-09 15:06:32 +0530

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,11 @@
+-- Last updated: 7/9/2026, 3:06:30 PM
+# Write your MySQL query statement below
+SELECT
+    id,
+    CASE
+        WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN student
+        WHEN id % 2 = 1 THEN LEAD(student) OVER (ORDER BY id)
+        ELSE LAG(student) OVER (ORDER BY id)
+    END AS student
+FROM Seat
+ORDER BY id
