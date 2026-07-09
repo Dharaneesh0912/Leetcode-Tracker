6886ce8cd23e72613a377a47b16e9095cb751fae
Dharaneesh0912 <dharaneeshs0912@gmail.com>
2026-07-09 15:09:48 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 3:09:46 PM
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char ch = strs[0][i];
+
+            for (int j = 1; j < strs.size(); j++) {
+                if (i >= strs[j].length() || strs[j][i] != ch) {
+                    return strs[0].substr(0, i);
+                }
+            }
+        }
+
+        return strs[0];
+    }
+};
