a935c6fe10300f6e7f55893894b71125959cf867
Dharaneesh0912 <dharaneeshs0912@gmail.com>
2026-09-22 09:15:10 +0530

    Create 434-NumberofSegmentsinaString

---
+++ 434-NumberofSegmentsinaString/version/python/434-NumberofSegmentsinaString_20260922_091508.py
@@ -0,0 +1,10 @@
+# Last updated: 9/22/2026, 9:15:08 AM
+1class Solution(object):
+2    def countSegments(self, s):
+3        input1=s.split()
+4        count=0
+5        print(input1)
+6        for i in input1:
+7            count+=1
+8        return count
+9
